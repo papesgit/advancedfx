655cd08180fe16d5340934e474505cf0ed2f3a8b
papesgit <lucas.wolf2@googlemail.com>
2026-08-18 17:44:49 +0200

    HOTv0.4.0

--- AfxHookSource2/HotVersion.h
+++ AfxHookSource2/HotVersion.h
@@ -1,5 +1,5 @@
 #pragma once
 
 // Update these values whenever a versioned HLAE-for-HOT package is produced.
-inline constexpr const char* HOT_COMPATIBILITY_VERSION = "0.3.2";
+inline constexpr const char* HOT_COMPATIBILITY_VERSION = "0.4.0";
 inline constexpr unsigned int HLAE_PACKAGE_REVISION = 1;
