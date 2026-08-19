686f34deced15a3dbf278f823f9a0afc954e63bc
papesgit <lucas.wolf2@googlemail.com>
2026-08-19 20:36:30 +0200

    Define upstream version directly in HotVersion.h

--- AfxHookSource2/HotVersion.h
+++ AfxHookSource2/HotVersion.h
@@ -1,5 +1,6 @@
 #pragma once
 
 // Update these values whenever a versioned HLAE-for-HOT package is produced.
+inline constexpr const char* HLAE_UPSTREAM_VERSION = "2.191.1";
 inline constexpr const char* HOT_COMPATIBILITY_VERSION = "0.4.0";
 inline constexpr unsigned int HLAE_PACKAGE_REVISION = 1;
